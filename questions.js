0ea553827c9035a3eebce39dccb340eb11f04e7a
Tarun-Kaushik01 <kaushiktarun880@gmail.com>
2026-07-06 00:12:44 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,37 @@
+const questions = [
+    {
+        id: 1,
+        question: "Which language is used with React?",
+        options: [
+            "Python",
+            "JavaScript",
+            "Java",
+            "C++"
+        ],
+        answer: "JavaScript"
+    },
+    {
+        id: 2,
+        question: "React is maintained by?",
+        options: [
+        "Google",
+        "Facebook",
+        "Microsoft",
+        "Amazon"
+        ],
+        answer: "Facebook"
+    },
+    {
+        id: 3,
+        question: "Which hook is used for state?",
+        options: [
+        "useRef",
+        "useMemo",
+        "useState",
+        "useContext"
+        ],
+        answer: "useState"
+    } 
+];
+
+export default questions;
